95cbb0c96d1182b1384987f9cc8fbf80a57c9242
khaiky <samanthagabitano015@gmail.com>
2024-11-01 21:01:29 +0800

    added text input for city and weather

--- Applet-5.js
+++ Applet-5.js
@@ -2,5 +2,10 @@ class WeatherApp {
     constructor() {
         // API Key
         this.apiKeyInput = document.getElementById('apiKeyInput'); 
+
+         // Text Input
+         this.cityInput = document.getElementById('cityInput');
+         this.getWeatherBtn = document.getElementById('getWeatherBtn');
+ 
     }
 }
