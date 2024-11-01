b2aef8917020ed5379c76bda737647b44fc929f8
khaiky <samanthagabitano015@gmail.com>
2024-11-01 21:03:01 +0800

    added weather card

--- Applet-5.js
+++ Applet-5.js
@@ -6,9 +6,17 @@ class WeatherApp {
          // Text Input
          this.cityInput = document.getElementById('cityInput');
          this.getWeatherBtn = document.getElementById('getWeatherBtn');
-         
+
         // Geolocation Input
         this.getLocationBtn = document.getElementById('getLocationBtn');
+
+          // Weather Card
+          this.weatherCard = document.getElementById('weatherCard');
+          this.cityName = document.getElementById('cityName');
+          this.temperature = document.getElementById('temperature');
+          this.description = document.getElementById('description');
+          this.humidity = document.getElementById('humidity');
+          this.windSpeed = document.getElementById('windSpeed');
  
     }
 }
