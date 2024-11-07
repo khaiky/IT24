cc0b679fed87f9af977bf6eb0cd85e90aa839a60
hiether17 <lhietherfhiona@gmail.com>
2024-11-08 00:56:30 +0800

    updated repository

--- Applet-2.js
+++ Applet-2.js
@@ -1,10 +1,10 @@
-
 class LeafletMap {
 
     constructor(containerId, center, zoom) {
         this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
     }
+
     initTileLayer() {
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -28,6 +28,21 @@ class LeafletMap {
             .catch(error => console.error('Error loading markers:', error));
     }
 }
-const myMap = new LeafletMap('map', [8.360004, 124.868419], 14);
 
-myMap.loadMarkersFromJson('applet-2.json');
+const myMap = new LeafletMap('map', [8.360004, 124.868419], 18);
+
+myMap.addMarker(8.359735, 124.869206, 'CCS Faculty Office');
+myMap.addMarker(8.359639,124.869179, 'CCS Laboratory 1');
+myMap.addMarker(8.359554,124.869153, 'CCS Laboratory 2');
+myMap.addMarker(8.307402,124.858806, 'Balay ni Sammy');
+myMap.addMarker(8.310116,124.858972, 'balay nila alliah');
+myMap.addMarker(8.313183, 124.862080, 'balay ni hiter');
+myMap.addMarker(8.317840,124.860649, 'balay nila jibson');
+myMap.addMarker(8.360219,124.867465, 'SC Building');
+myMap.addMarker(8.360118,124.868904, 'Covered Court');
+myMap.addMarker(8.360123,124.869306, 'AVR');
+myMap.addMarker(8.359141,124.868581, 'BSBA Building');
+myMap.addMarker( 8.359269,124.867835, 'Admin Building');
+
+
+myMap.loadMarkersFromJson('Applet-2.json');
